abe507b27c6ed08b7a328f0f5dcaa3d4a2fed6a5
pchinjr <pchinjr@gmail.com>
2016-02-17 13:21:32 -0500

    initial commit, works, but don't have a browser open and pointing at local host before you start the server

---
+++ server.js
@@ -0,0 +1,79 @@
+// server.js
+var express = require('express');
+var app = express();
+var server = require("http").createServer(app);
+var five = require("johnny-five");
+var io=require('socket.io')(server);
+ 
+var port = 3000;
+ 
+app.use(express.static(__dirname + '/public'));
+ 
+app.get('/', function(req, res) {
+        res.sendFile(__dirname + '/public/index.html');
+});
+ 
+server.listen(port);
+console.log('Server available at http://localhost:' + port);
+
+var led;
+ 
+//Arduino board connection
+ 
+var board = new five.Board();
+board.on("ready", function() {
+    led = new five.Led(13);
+    servo = new five.Servo({
+      pin: 10,
+      center: true
+    });
+    photoresistor = new five.Sensor({
+      pin:'A0',
+      freq: 1000
+    });
+    console.log('Arduino connected');
+});
+ 
+//Socket connection handler
+io.on('connection', function (socket) {
+        console.log("your socket id is ", socket.id);
+        
+        socket.on('join', function(data) {
+          console.log('browser connected');
+        });
+ 
+        socket.on('led:on', function() {
+           led.on();
+           console.log('LED ON RECEIVED');
+        });
+ 
+        socket.on('led:off', function () {
+            led.stop().off();
+            console.log('LED OFF RECEIVED');
+        });
+        
+        socket.on('led:blink', function () {
+            led.blink(300);
+            console.log('LED BLINK RECEIVED');
+        });
+        
+        socket.on('servo:sweep', function () {
+            servo.sweep();
+            console.log('SERVO SWEEP RECEIVED');
+        });
+        
+        socket.on('servo:stop', function () {
+            servo.stop();
+            console.log('SERVO STOP RECEIVED');
+        });
+        
+        photoresistor.on('data', function() {
+          socket.emit('push', this.value);
+        });
+        
+        socket.on('disconnect', function() {
+          console.log('browser closed');
+        })
+    });
+ 
+console.log('Waiting for arduino connection..');
