e3c9a1f41ffe99b83ece8861a84ad9f10ade4f8d
Paul Chin Jr <pchinjr@Pauls-MBP.home>
2016-04-06 10:47:02 -0400

    dark worship

--- server.js
+++ server.js
@@ -4,22 +4,18 @@ var app = express();
 var server = require("http").createServer(app);
 var five = require("johnny-five");
 var io = require('socket.io')(server);
-
 var port = 3000;
 
+//create the node server and serve the files
 app.use(express.static(__dirname + '/public'));
-
 app.get('/', function(req, res) {
         res.sendFile(__dirname + '/public/index.html');
 });
-
 server.listen(port);
 console.log('Cage Bot is now available to worship at http://localhost:' + port);
 
-var led;
-
 //Arduino board connection
-
+var led;
 var board = new five.Board();
 board.on("ready", function() {
     led = new five.Led(13);
@@ -34,7 +30,7 @@ board.on("ready", function() {
     console.log('Arduino connected');
 });
 
-//Socket connection handler
+//SocketIO connection handler and listening
 io.on('connection', function (socket) {
         console.log("your socket id is ", socket.id);
 
@@ -75,5 +71,6 @@ io.on('connection', function (socket) {
           console.log('browser closed');
         })
     });
-
+    
+//if nothing else is happened yet.
 console.log('Waiting for arduino connection..');
