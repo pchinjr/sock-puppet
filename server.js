26a41b0d8fef0d1511caaff64bcdc6a99ca1b982
Paul Chin <pchinjr@gmail.com>
2016-11-02 22:31:57 -0400

    set servo to 90

--- server.js
+++ server.js
@@ -36,9 +36,9 @@ var socket2 = io2.connect('http://45.55.86.193:3000/');
 
 socket2.on('connect', function(socket) {
         socket2.emit('cage-pi-connect', {message: 'connection established from cage'});
-        socket2.on('servo:sweep', function () {
-            servo.sweep();
-            console.log('SERVO SWEEP RECEIVED');
+        socket2.on('servo:set', function () {
+            servo.to(90);
+            console.log('SERVO SET TO 90 RECEIVED');
         });
 });
 
